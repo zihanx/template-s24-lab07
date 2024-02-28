94ec3866f2040aa5be2da83b875c8aa7822d6d9d
ZAN <zanorz233@gmail.com>
2024-02-28 02:03:15 -0500

    Implement recentmistakes

--- src/ordering/prioritization/recentmistakes.ts
+++ src/ordering/prioritization/recentmistakes.ts
@@ -2,23 +2,21 @@ import { CardStatus } from '../../cards/cardstatus.js'
 import { CardOrganizer } from '../cardorganizer.js'
 
 function newRecentMistakesFirstSorter (): CardOrganizer {
-  /**
-   * Computes the most recent mistake's time stamp for a card and helps in
-   * determining the sequence of cards in the next iteration, based on the
-   * rules that those answered incorrectly in the last round appear first.
-   *
-   * @param cardStatus The {@link CardStatus} object with failing
-   * @return The most recent incorrect response time stamp
-   */
+  // /**
+  //  * Computes the most recent mistake's time stamp for a card and helps in
+  //  * determining the sequence of cards in the next iteration, based on the
+  //  * rules that those answered incorrectly in the last round appear first.
+  //  *
+  //  * @param cardStatus The {@link CardStatus} object with failing
+  //  * @return The most recent incorrect response time stamp
+  //  */
   return {
-    /**
-     * Orders the cards by the time of most recent incorrect answers provided for them.
-     *
-     * @param cards The {@link CardStatus} objects to order.
-     * @return The ordered cards.
-     */
     reorganize: function (cards: CardStatus[]): CardStatus[] {
-      return []
+      return cards.sort((a, b) => {
+        const aResult = a.getResults().at(-1) ?? false
+        const bResult = b.getResults().at(-1) ?? false
+        return (aResult === bResult) ? 0 : aResult ? 1 : -1
+      })
     }
   }
 };
